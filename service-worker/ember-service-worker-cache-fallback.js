e14a0f9f630954caf1291576ecec41ec409b60c6
Heather Brysiewicz <h.a.brysiewicz@gmail.com>
2016-06-29 09:29:08 -0400

    Add no-cors mode

--- service-worker/ember-service-worker-cache-fallback.js
+++ service-worker/ember-service-worker-cache-fallback.js
@@ -1,7 +1,7 @@
 self.addFetchListener(function(event) {
   return caches.open('esw-fallback-cache')
     .then(function(cache) {
-      return fetch(event.request)
+      return fetch(event.request, { mode: 'no-cors' })
         .then(function(response) {
           cache.put(event.request, response.clone());
           return response;
